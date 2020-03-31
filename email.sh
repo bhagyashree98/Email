40193549cca75e8e6dffef0ee6c4f6a9b0822808
bhagyashree98 <bnsingasane@gmail.com>
2020-03-31 21:37:41 +0530

    First Commit

--- email.sh
+++ email.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 
+echo "Enter Email address"
+read email
+emailpat="^[0-9a-zA-Z]";
+if `[[ $email =~ $emailpat ]]`
+then
+	echo "Valid email address"
+else
+	echo "Invalid email address"
+fi
