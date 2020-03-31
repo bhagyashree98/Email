f9c09fe9cca51d1ccd53dbe475039f3b60d1958e
bhagyashree98 <bnsingasane@gmail.com>
2020-03-31 23:23:56 +0530

    First Commit

--- email.sh
+++ email.sh
@@ -2,7 +2,7 @@
 
 echo "Enter Email address"
 read email
-emailpat="[._+-][0-9a-zA-Z]";
+emailpat="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$";
 if `[[ $email =~ $emailpat ]]`
 then
 	echo "Valid email address"
